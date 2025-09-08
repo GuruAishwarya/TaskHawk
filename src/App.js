af47d6da3aee733b316b25f1b8e14dacb769fe7b
suryaSekar6 <suryasekar626@gmail.com>
2025-09-08 21:42:17 +0530

    added aboutus

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GetInTouch from './pages/getintouch';
 import './App.css';
+import AboutUs from './pages/About';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<GetInTouch />} />
           <Route path="/contact" element={<GetInTouch />} />
+          <Route path='/about' element={<AboutUs />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <h1>Surya</h1>
+  );
+};
+
+export default AboutUs;
