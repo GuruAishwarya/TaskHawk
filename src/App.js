24dd61acf5ec2e4791126969c533eee5aa358090
Rubini <rubinihari01@gmail.com>
2025-09-12 21:18:23 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-// import React from 'react';
-// import './App.css';
-// import Teamwork from './pages/Teamwork';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Teamwork />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import './App.css';
-import ProfilePage from './pages/ProfilePage';
+import Teamwork from './pages/Teamwork';
 
 function App() {
   return (
     <div className="App">
-      <ProfilePage />
+      <Teamwork />
     </div>
   );
 }
 
 export default App;
+
+// import React from 'react';
+// import './App.css';
+// import ProfilePage from './pages/ProfilePage';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <ProfilePage />
+//     </div>
+//   );
+// }
+
+// export default App;
